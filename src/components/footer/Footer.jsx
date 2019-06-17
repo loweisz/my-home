8eae7b296892976e76ac19017bf7223760632566
Lorenz Weiß <lorenz.weiss@loopline-systems.com>
2019-06-17 10:55:32 +0200

    disable image for built on footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import * as SC from '../header/header.sc'
-
-import github from '../../images/github_new.svg';
-import linkedIn from '../../images/linked_new.svg';
-import twitter from '../../images/twitter_new.svg';
-import spotify from '../../images/spotify_new.svg';
-import avatar from '../../images/avatar.jpg';
+// import github from '../../images/github_new.svg';
+// import linkedIn from '../../images/linked_new.svg';
+// import twitter from '../../images/twitter_new.svg';
+// import spotify from '../../images/spotify_new.svg';
+// import avatar from '../../images/avatar.jpg';
 
 const Footer = () => (
     <SC.InfoHeader>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import * as SC from './header.sc'
-
-import github from '../../images/github_new.svg';
-import linkedIn from '../../images/linked_new.svg';
-import twitter from '../../images/twitter_new.svg';
-import spotify from '../../images/spotify_new.svg';
+//
+// import github from '../../images/github_new.svg';
+// import linkedIn from '../../images/linked_new.svg';
+// import twitter from '../../images/twitter_new.svg';
+// import spotify from '../../images/spotify_new.svg';
 
 const Header = () => (
     <SC.InfoHeader>
