72e98493e770a4b9c0286f5eed166e4a84181679
Lorenz <lorenz.weis@gmail.com>
2020-03-26 15:47:13 +0100

    code styling in blogs

--- src/components/dev/experience.styles.js
+++ src/components/dev/experience.styles.js
@@ -153,7 +153,7 @@ export const DataText = styled.div`
   }
 
   code[class*='language-'] {
-    color: ${({ theme }) => theme.grey};
+    color: ${({ theme }) => theme.white};
   }
 
   a {
@@ -166,126 +166,132 @@ export const DataText = styled.div`
     padding: 1rem;
     margin-top: -5px;
   }
-  
+
   pre[class*='language-']::-moz-selection {
     /* Firefox */
     background: hsl(207, 4%, 16%);
   }
-  
+
   pre[class*='language-']::selection {
     /* Safari */
     background: hsl(207, 4%, 16%);
   }
-  
+
   /* Text Selection colour */
   pre[class*='language-']::-moz-selection,
   pre[class*='language-'] ::-moz-selection {
     text-shadow: none;
     background: hsla(0, 0%, 100%, 0.15);
   }
-  
+
   pre[class*='language-']::selection,
   pre[class*='language-'] ::selection {
     text-shadow: none;
     background: hsla(0, 0%, 100%, 0.15);
   }
-  
+
   /* Inline code */
   :not(pre) > code[class*='language-'] {
     border-radius: 0.3em;
     background: black;
-    color: #CB772F;
+    color: #cb772f;
     padding: 0.15em 0.2em 0.05em;
     white-space: normal;
   }
-  
+
   .token.attr-name {
     color: rgb(173, 219, 103);
     font-style: italic;
   }
-  
+
   .token.comment {
     color: rgb(128, 147, 147);
   }
-  
+
   .token.string,
   .token.url {
     color: rgb(173, 219, 103);
   }
-  
+
   .token.variable {
     color: rgb(214, 222, 235);
   }
-  
+
   .token.number {
     color: rgb(247, 140, 108);
   }
-  
+
   .token.builtin,
   .token.char,
   .token.constant,
   .token.function {
     color: rgb(130, 170, 255);
   }
-  
+
   .token.punctuation {
     color: rgb(199, 146, 234);
   }
-  
+
   .token.selector,
   .token.doctype {
     color: rgb(199, 146, 234);
   }
-  
+
   .token.class-name {
     color: rgb(255, 203, 139);
   }
-  
+
   .token.tag,
   .token.operator,
   .token.keyword {
     color: #ba6e73;
   }
-  
+
   .token.boolean {
     color: rgb(255, 88, 116);
   }
-  
+
   .token.property {
     color: rgb(128, 203, 196);
   }
-  
+
   .token.namespace {
     color: rgb(178, 204, 214);
   }
-  
+
   pre[data-line] {
     padding: 1em 0 1em 3em;
     position: relative;
   }
-  
+
   .gatsby-highlight-code-line {
-    background-color: hsla(0,0%,80%, 0.1);
+    background-color: hsla(0, 0%, 80%, 0.1);
     display: block;
     padding-right: 1em;
     padding-left: 1.25em;
   }
-  
+
   .gatsby-highlight {
     margin-bottom: 1.75rem;
     border-radius: 10px;
-    background: #320A28;
+    background: #320a28;
     -webkit-overflow-scrolling: touch;
     overflow: auto;
   }
-  
+
   @media (max-width: 672px) {
     .gatsby-highlight {
       border-radius: 0;
     }
   }
-  
+
   .gatsby-highlight pre[class*='language-'] {
     float: left;
     min-width: 100%;
+  }
+  p {
+    > code {
+      color: ${({ theme }) => theme.darkRed} !important;
+    }
+  }
 `;
