148755bb8829fd1a81792b509af1e2f9c8d122bd
Lorenz <lorenz.weis@gmail.com>
2020-04-14 18:43:06 +0200

    dark theme toggle stylings

--- src/components/themeToggle/themeToggle.style.js
+++ src/components/themeToggle/themeToggle.style.js
@@ -1,35 +1,36 @@
 import styled, { keyframes } from 'styled-components';
 
 export const IconContainer = styled.div`
-  height: 40px;
-  width: 40px;
+  height: 60px;
+  width: 60px;
   flex: 1;
 
   position: relative;
   > svg {
     position: absolute;
     transition: all 500ms ease;
-    left: 8px;
+    left: 14px;
   }
   .moon {
-    margin-top: ${({ shown }) => (shown ? '-40px' : '6px')};
+    margin-top: ${({ shown }) => (shown ? '-60px' : '10px')};
   }
   .sun {
-    margin-top: ${({ shown }) => (!shown ? '-40px' : '6px')};
+    margin-top: ${({ shown }) => (!shown ? '-60px' : '10px')};
   }
 `;
 
 export const ThemeButton = styled.button`
   position: fixed;
-  bottom: 24px;
-  left: 12px;
+  bottom: 16px;
+  left: 0;
   border: none;
-  font-size: 24px;
+  font-size: 34px;
   margin-left: 24px;
   background: ${({ theme }) => theme.black};
+  transition: background 500ms ease;
   border-radius: 50%;
-  height: 40px;
-  width: 40px;
+  height: 60px;
+  width: 60px;
   display: flex;
   justify-content: center;
   flex-direction: column;
