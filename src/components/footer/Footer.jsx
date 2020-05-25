3cdab4cedae201187b743abe62424207bd9ff479
lowe1111 <lorenz.weiss@loopline-systems.com>
2020-05-25 21:25:56 +0200

    footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <StyledFooter>
       <FooterInner>
         <ThemeToggle />
-        <span>2020 Made by Lorenz Weiß.</span>
+        <span>2020 Built and designed by Lorenz Weiß.</span>
       </FooterInner>
     </StyledFooter>
   );
