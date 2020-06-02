70112ff781307b58404de3a635e729c5136592ca
lowe1111 <lorenz.weiss@loopline-systems.com>
2020-06-02 17:53:13 +0200

    create page for newsletter

---
+++ src/pages/newsletter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Wrapper, InfoStarter, TextBlock, HeaderText, TextSection } from '../styles/pages.sc';
+import Layout from '../components/layout';
+import SEO from '../components/seo.helper';
+import Newsletter from '../components/newsletter/Newsletter';
+
+const NewsletterPage = () => (
+  <Layout>
+    <SEO title="Projects" description="Subscribe to my newsletter" />
+    <Wrapper>
+      <InfoStarter>
+        <TextSection>
+          <Newsletter />
+        </TextSection>
+      </InfoStarter>
+    </Wrapper>
+  </Layout>
+);
+
+export default NewsletterPage;
