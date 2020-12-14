640c6761df6b61082f0819aaa82c8560ee0f4c7e
lowe1111 <lorenz.weiss@loopline-systems.com>
2020-12-14 22:43:58 +0100

    major updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,9 +103,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
     {
       resolve: `gatsby-styled-components-dark-mode`,
       options: {
